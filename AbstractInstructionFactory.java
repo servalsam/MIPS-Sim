9488c5fee6b45ce09c626b93a95d31fafa686cd9
servalsam <51174373+servalsam@users.noreply.github.com>
2020-10-25 14:57:14 -0700

    Update AbstractInstructionFactory.java

--- AbstractInstructionFactory.java
+++ AbstractInstructionFactory.java
@@ -4,7 +4,7 @@ package mipssim.swain91.git;
  * Base interface for {@link RegisterInstructionFactory}, {@link ImmediateInstructionFactory},
  * and {@link JumpInstructionFactory}. 
  * 
- * @author Samuel Wainright
+ * @author Samuel Servane
  * @version 1.3
  */
 public interface AbstractInstructionFactory {
