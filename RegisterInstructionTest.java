f5828a513bb9f1c1d4b032cd0fa201164570a10a
servalsam <51174373+servalsam@users.noreply.github.com>
2020-10-25 15:03:05 -0700

    Update RegisterInstructionTest.java

--- RegisterInstructionTest.java
+++ RegisterInstructionTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.BeforeAll;
 /**
  * Test class for the {@link RegisterInstruction} class.
  * 
- * @author Samuel Wainright
+ * @author Samuel Servane
  * @version 1.3
  */
 public class RegisterInstructionTest {
