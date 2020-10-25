677fcd46b58a3c59f0fad862fdeeae76f5aa23fd
servalsam <51174373+servalsam@users.noreply.github.com>
2020-10-25 15:01:59 -0700

    Update InstructionFactoryTest.java

--- InstructionFactoryTest.java
+++ InstructionFactoryTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 /**
  * Test class for the {@link InstructionFactory} class.
  * 
- * @author Samuel Wainright
+ * @author Samuel Servane
  * @version 1.3
  */
 public class InstructionFactoryTest {
